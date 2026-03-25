b77412f6490c3503c1ab69e912ff62f42214dad6
Syed Rizwan <118912602+imSRAQ@users.noreply.github.com>
2026-03-25 16:34:09 +0530

    Add solution for make-diagonal

---
+++ make-diagonal/make-diagonal.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def make_diagonal(v):
+    """
+    Returns: (n, n) NumPy array with v on the main diagonal
+    """
+
+    v = np.asarray(v, dtype=float)
+
+    vec_v_len = len(v)
+    zeros_matrix = np.zeros((vec_v_len, vec_v_len))
+
+    for idx_i, i in enumerate(zeros_matrix):
+        for idx_j, j in enumerate(i):
+            if idx_i == idx_j:
+                zeros_matrix[idx_i][idx_j] = v[idx_i]
+
+    diag_matrix = zeros_matrix
+    return diag_matrix
+    
